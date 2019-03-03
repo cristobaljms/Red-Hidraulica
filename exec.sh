6c70056d9d697a06013dcdd3653ad254f6ddafca
Cristobal <cmunoz21x@gmail.com>
2019-03-03 18:21:04 -0400

    se aadio doc

--- exec.sh
+++ exec.sh
@@ -1,2 +1,2 @@
 python manage.py runserver
-celery -A root worker -l info
+celery -A root worker -l info -P eventlet
--- root/settings.py
+++ root/settings.py
@@ -160,5 +160,5 @@ CELERY_RESULT_BACKEND = 'redis://localhost:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
-# BROKER_URL = 'amqp://guest:guest@localhost//'
-# CELERY_RESULT_BACKEND = 'amqp://guest:guest@localhost//'
+#BROKER_URL = 'amqp://guest:guest@localhost//'
+#CELERY_RESULT_BACKEND = 'amqp://guest:guest@localhost//'
